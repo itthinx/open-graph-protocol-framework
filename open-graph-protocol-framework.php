18a87a2d37f762fa4f34d847cfdd1e590def7286
itthinx <itthinx@itthinx.com>
2025-06-05 13:39:06 +0200

    2025

--- open-graph-protocol-framework.php
+++ open-graph-protocol-framework.php
@@ -11,7 +11,7 @@
  * Donate-Link: https://www.itthinx.com/shop/
  * License: GPLv3
  *
- * Copyright (c) 2012 - 2024 "kento" Karim Rahimpur www.itthinx.com
+ * Copyright (c) 2012 - 2025 "kento" Karim Rahimpur www.itthinx.com
  *
  * This code is released under the GNU General Public License Version 3.
  * The following additional terms apply to all files as per section
